71835f69c36bff2c1bdd1c03e15c5db1487ae8ef
Lorenzo Betamin <163295042+Assitip@users.noreply.github.com>
2024-09-09 17:09:14 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,48 @@
+let dados = [
+    {
+        titulo : '718 Cayman',
+        descricao : 'O Porsche 718 Cayman 2025 redefine os limites do desempenho em um pacote compacto e elegante. Com um motor bóxer biturbo ainda mais potente e uma aerodinâmica aprimorada, o Cayman oferece uma experiência de condução emocionante a cada curva. A adição de sistemas de assistência avançados e um interior high-tech garantem que você esteja conectado e seguro a cada momento.',
+        link : 'https://configurator.porsche.com/pt-BR/model/982120',
+        tags: 'carros carro porsche rápido rapido'
+    }, 
+    {
+        titulo : '911 Carrera',
+        descricao : 'O Porsche 911 Carrera 2025 é a mais recente evolução de um ícone. Mantendo a alma de seus antecessores, este esportivo combina a tradição com a inovação. Seu motor boxer biturbo oferece um desempenho emocionante, enquanto a tecnologia de ponta garante uma experiência de condução ainda mais refinada. O 911 Carrera 2025 é a prova de que a perfeição pode ser aprimorada.',
+        link : 'https://configurator.porsche.com/pt-BR/model/9921B2',
+        tags: 'carros carro porsche rápido rapido'
+    }, 
+    {
+        titulo : 'Taycan',
+        descricao : 'O Porsche Taycan 2025 redefine os padrões de desempenho em veículos elétricos. Com motores elétricos ainda mais potentes e uma bateria de última geração, o Taycan oferece uma aceleração vertiginosa e uma autonomia estendida. A adição de sistemas de assistência avançados e um interior high-tech garantem uma experiência de condução emocionante e conectada.',
+        link : 'https://configurator.porsche.com/pt-BR/model/Y1AAI',
+        tags: 'carros carro porsche rápido rapido eletrico elétrico'
+    },
+    {
+        titulo: 'Macan',
+        descricao: 'O Porsche Macan 2025 combina a agilidade de um SUV compacto com o desempenho de um esportivo. Seu design elegante e esportivo, aliado a um motor potente e um chassi preciso,     proporcionam uma experiência de condução dinâmica e confortável. O Macan é a escolha perfeita para quem busca um SUV de luxo com alma esportiva.',
+        link: 'https://configurator.porsche.com/pt-BR/model/95BAU1',
+        tags: 'carros carro porsche suv rapido crossover'
+    },
+
+
+    {
+        titulo: 'Panamera',
+        descricao: 'O Porsche Panamera 2025 redefine o conceito de grand tourer. Com um design elegante e esportivo, um interior luxuoso e um desempenho excepcional, o Panamera oferece uma     experiência de condução única. Seus motores potentes e a suspensão adaptativa garantem um conforto incomparável, mesmo em longas viagens.',
+        link: 'https://configurator.porsche.com/pt-BR/model/970110',
+        tags: 'carros carro porsche sedan luxo grandtourer'
+    },
+
+
+    {
+        titulo: '718 Boxster',
+        descricao: 'O Porsche 718 Boxster 2025 é o roadster mais puro da Porsche. Com um design atemporal e um motor boxer biturbo posicionado centralmente, o Boxster oferece uma experiência de     condução emocionante e visceral. Seu chassi leve e ágil garante uma conexão direta com a estrada, proporcionando um prazer de dirigir incomparável.',
+        link: 'https://configurator.porsche.com/pt-BR/model/982110',
+        tags: 'carros carro porsche roadster esportivo conversível'
+    },
+    {
+        titulo: '911 Turbo S',
+        descricao: 'O Porsche 911 Turbo S 2025 é o ápice da engenharia automotiva. Com um motor biturbo de seis cilindros boxer, o Turbo S oferece um desempenho brutal e uma agilidade incomparável. Seu sistema de tração integral e aerodinâmica ativa garantem a máxima aderência em qualquer condição.',
+        link: 'https://configurator.porsche.com/pt-BR/model/9921B6',
+        tags: 'carros carro porsche rapido super carro esportivo'
+    }
+];
